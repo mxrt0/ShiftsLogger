7eb3aa975dc259d094392c93b7b06fd10f5cb937
Martin Terziev <m.terziev08@abv.bg>
2025-08-15 10:08:04 +0300

    Removing sensitive cnn string.

--- ShiftsLogger.API/Program.cs
+++ ShiftsLogger.API/Program.cs
@@ -1,27 +1,31 @@
-namespace ShiftsLogger.API
+using Microsoft.EntityFrameworkCore;
+using ShiftsLogger.Data.Context;
+
+namespace ShiftsLogger.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        // Add services to the container.
 
-            builder.Services.AddControllers();
+        builder.Services.AddDbContext<ShiftsDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString())
+        builder.Services.AddControllers();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+        // Configure the HTTP request pipeline.
 
-            app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+        app.UseAuthorization();
 
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.Run();
-        }
+        app.Run();
     }
 }
